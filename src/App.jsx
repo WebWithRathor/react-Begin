91504c0993fed3bd8b7730c0e328a7710f91e45c
WebWithRathor <devrajrathor143@gmail.com>
2024-05-06 21:42:54 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import {useRef , useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  function handle(e) {
+    e.preventDefault();
+    const newuser = { username, textarea, Gender, City }
+    setarray([...array, newuser]);
+    setCity('Bhopal');
+    setGender('male');
+    setUsername("");
+    settextarea("");
+  }
 
+
+
+  const [array, setarray] = useState([])
+  const [username, setUsername] = useState('');
+  const [textarea, settextarea] = useState('');
+  const [Gender, setGender] = useState('male')
+  const [City, setCity] = useState('Bhopal');
+  let listner = array.map((array, i) => {
+    return (
+      <div key={i}>
+        <h1 >{array.username}</h1>
+        <h1>{array.textarea}</h1>
+        <h1>{array.Gender}</h1>
+        <h1>{array.City}</h1>
+      </div>
+    )
+  })
+  const wow = useRef(null)
+  setTimeout(() => {
+    wow.current.innerHTML = 'nhi btaung'
+  }, 1000);
+  console.log(listner)
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 ref={wow} className="text-red-500 font-medium" >Form on react</h1>
+      <form onSubmit={handle} action="">
+        <input type="radio" onChange={(e) => setGender(e.target.value)} value='male' checked={Gender === 'male' ? true : false} name="a" id="" /> <label htmlFor=''>Male</label>
+        <input type="radio" onChange={(e) => setGender(e.target.value)} value='female' checked={Gender === 'female' ? true : false} name="a" id="" /> <label htmlFor="">Female</label><br /><br /><br /><br />
+        <input onChange={(e) => setUsername(e.target.value)} value={username} type="text" /><br /><br /><br />
+        <textarea onChange={(e) => settextarea(e.target.value)} value={textarea} name="" id="" cols="30" rows="3"></textarea><br /><br />
+        <select onChange={(e) => setCity(e.target.value)} value={City} name="" id="">
+          <option value="Delhi">delhii</option>
+          <option value="Bhopal">bhopal</option>
+        </select><br /><br />
+        <button>submit</button>
+      </form>
+      {listner}
     </>
   )
 }
-
-export default App
+export default App;
